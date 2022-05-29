1740036ba699203661e8910427e62c6fa76f205d
SERGIO RIVEROS <sergiodamian015@gmail.com>
2022-05-29 19:24:12 -0400

    se agrega la barra de busqueda en el front y se modifica el servicio de reservas get con parameetros

--- app/routes/reservas.routes.js
+++ app/routes/reservas.routes.js
@@ -3,7 +3,7 @@ module.exports = app => {
     var router = require("express").Router();
     router.post("/", reserva.create);
     router.get("/", reserva.findAll);
-    router.get("/filter", reserva.filter);
+    router.get("/filter/:restauranteId/:fecha/:clienteId", reserva.filter);
     router.get("/:id", reserva.findOne);
     router.put("/:id", reserva.update);
     router.delete("/:id", reserva.delete);
--- front-end/src/reserva/listarReserva.js
+++ front-end/src/reserva/listarReserva.js
@@ -1,27 +1,27 @@
 import axios from "axios";
 //import { use } from "express/lib/router";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-const URI='http://localhost:9090/api/reservas'
+const URI = 'http://localhost:9090/api/reservas'
 
-const CompListarReservas=() =>{
-    const [reservas,setReservas]=useState([])
-    useEffect(() =>{
+const CompListarReservas = () => {
+    const [reservas, setReservas] = useState([])
+    useEffect(() => {
         getReservas()
-    },[])
+    }, [])
 
     //procedimiento para mostrar todas las Reservas
 
-    const getReservas = async() =>{
-       const res = await axios.get(URI)
-       setReservas(res.data)
+    const getReservas = async () => {
+        const res = await axios.get(URI)
+        setReservas(res.data)
     }
 
 
-    const deleteReservas = async(id) =>{
-        
-       await axios.delete(URI+'/'+id)
+    const deleteReservas = async (id) => {
+
+        await axios.delete(URI + '/' + id)
         getReservas()
     }
 
@@ -30,6 +30,19 @@ const CompListarReservas=() =>{
             <div className="row">
                 <div className="col">
                     <th>Lista de Reservas</th>
+                    <div className="colum">
+                        <nav class="navbar bg-light">
+                            <div class="container-fluid">
+                                <form class="d-flex" role="search">
+                                    <input class="form-control me-2" type="search" value={""}
+                                        placeholder="Restaurante" aria-label="Search" />
+                                    <input class="form-control me-2" type="date"
+                                        placeholder="Search" aria-label="Search" value={""}/>
+                                    <button class="btn btn-outline-success" type="submit">Buscar</button>
+                                </form>
+                            </div>
+                        </nav>
+                    </div>
                     <table className="table">
                         <thead className="table-primary">
                             <tr>
@@ -43,14 +56,14 @@ const CompListarReservas=() =>{
                             </tr>
                         </thead>
                         <tbody>
-                            {reservas.map ((reserva)=>(
+                            {reservas.map((reserva) => (
                                 <tr key={reserva.id}>
                                     <td>{reserva.id}</td>
                                     <td>{reserva.Restaurante.nombre}</td>
                                     <td>{reserva.MesaMesaId}</td>
                                     <td>{reserva.cantidad}</td>
                                     <td>{reserva.ClienteId}</td>
-                                    <td placeholder="dd-mm-yyyy">{reserva.fecha}</td>
+                                    <td>{reserva.fecha}</td>
                                     <td>{reserva.horario}</td>
                                 </tr>
                             ))}
