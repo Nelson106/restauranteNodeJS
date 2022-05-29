2d604dcef3c466e9e38fdeead974deb6d7393190
Nelson <ruizn291@gmail.com>
2022-05-28 20:10:40 -0400

    actualizar mesa

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import CompListarMesa from './mesas/listarMesa';
 import CompCrearMesa from './mesas/crearMesa';
 import { BrowserRouter,Route, Routes } from 'react-router-dom';
+import CompEditarMesa from './mesas/actualizarMesa';
 
 //import { Router } from 'express';
 function App() {
@@ -17,7 +18,7 @@ function App() {
             <Routes>
               <Route path='/' element={<CompListarMesa />} />
               <Route path='/create' element={ <CompCrearMesa /> } />
-              
+              <Route path='/update/:mesaId' element={ <CompEditarMesa /> } />
             </Routes>
         </BrowserRouter>
      
---
+++ front-end/src/restaurante/listarRestaurante.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+//import { use } from "express/lib/router";
+import { useState,useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const URI='http://localhost:9090/api/mesas'
